19d6ecb8b9ec314707fbd02c81611c7e6dfac2c7
Jean Braz <jeanbrazcosta@gmail.com>
2026-05-03 20:38:52 -0300

    chore(addon-db): update addon version to 2025.1.0 and modify URL and publisher details

--- buildVars.py
+++ buildVars.py
@@ -25,7 +25,7 @@ addon_info = {
 	# Translators: Long description to be shown for this add-on on add-on information from add-ons manager
 	"addon_description" : _("""Enhance the speech and braille support while using the Eclipse IDE reportings errors and warnings."""),
 	# version
-	"addon_version" : "2025.1",
+	"addon_version" : "2025.1.0",
 	# Author(s)
 	"addon_author" : u"Jean Braz <jeanbrazcosta@gmail.com>",
 	# URL for the add-on documentation support
